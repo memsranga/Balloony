bc4b065fbd455d589160b0c9371814b4d8b06a82
Shanmukha Ranganath <shanmukharanganath@gmail.com>
2019-07-22 01:26:59 +0530

    Balloon animation

--- Balloony/MainPage.xaml.cs
+++ Balloony/MainPage.xaml.cs
@@ -1,22 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
 using static Balloony.TouchEffect;
-using System.IO;
-using System.Reflection;
 
 namespace Balloony
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
@@ -111,8 +100,6 @@ namespace Balloony
 
             canvas.Clear();
 
-            var sliderThumbX = (float)(info.Width * Percent / 100);
-            var sliderY = info.Height - 50;
             DrawSlider(canvas, info, Percent);
             DrawThumb(canvas, info, Percent, _touchType);
         }
@@ -121,7 +108,6 @@ namespace Balloony
         {
             if (this.AnimationIsRunning("TranslationAnimation"))
             {
-                // this.AbortAnimation("TranslationAnimation");
                 return;
             }
 
@@ -129,29 +115,18 @@ namespace Balloony
             var newX = balloon_slider.Width * percent / 100 - balloonSvg.Width / 2;
 
 
-            // increase angle based on delta
             var translation = new Animation();
             translation.Add(0, 1, new Animation((s) =>
             {
-                // Debug.WriteLine("new translation: " + oldX + s * Math.Abs(oldX - newX));
-
                 if (oldX > newX)
                 {
-                    var delta = oldX - s * Math.Abs(oldX - newX);// balloon_slider.Width * percent / 100;;
+                    var delta = oldX - s * Math.Abs(oldX - newX);
                     balloonSvg.TranslationX = delta;
-                    var angle = Math.Abs(oldX - newX) > 0.001 ? Math.Tanh(delta) : 0;
-                    Debug.WriteLine("angle: " + angle);// s * delta);
-                    balloonSvg.Rotation = s * angle * 45;// s * delta;
                 }
                 else
                 {
-                    var delta = oldX + s * Math.Abs(oldX - newX);// balloon_slider.Width * percent / 100;
-
+                    var delta = oldX + s * Math.Abs(oldX - newX);
                     balloonSvg.TranslationX = delta;
-                    var angle = Math.Abs(oldX - newX) > 0.001 ? Math.Tanh(delta) : 0;
-                    Debug.WriteLine("angle: " + angle);// - s * delta);
-                    balloonSvg.Rotation = (1 - s) * angle * 45;// - s * delta;
-                    // baloonSvg.Rotation = (1 - s) * 45;
                 }
             }, 0, 1));
             translation.Add(0, 1, new Animation(s =>
@@ -159,18 +134,18 @@ namespace Balloony
                 if (oldX > newX)
                 {
                     var delta = oldX - s * Math.Abs(oldX - newX);
-                    var angle = Math.Abs(oldX - newX) > 0.001 ? Math.Tanh(delta) : 0;
-                    balloonSvg.Rotation = s * angle * 45;
+                    var angle = Math.Abs(oldX - newX) > 0.001 ? Math.Tanh((oldX - newX) / balloon_slider.Width) : 0;
+                    balloonSvg.Rotation = angle * 180;
                 }
                 else
                 {
                     var delta = oldX + s * Math.Abs(oldX - newX);
-                    var angle = Math.Abs(oldX - newX) > 0.001 ? Math.Tanh(-delta) : 0;
-                    balloonSvg.Rotation = s * angle * 45;
+                    var angle = Math.Abs(oldX - newX) > 0.001 ? Math.Tanh((oldX - newX) / balloon_slider.Width) : 0;
+                    balloonSvg.Rotation = angle * 180;
                 }
             }, 0, 1, finished: () =>
             {
-                balloonSvg.RelRotateTo(-balloonSvg.Rotation, 1000);
+                balloonSvg.RelRotateTo(-balloonSvg.Rotation, 500);
             }));
             translation.Commit(balloonSvg, "TranslationAnimation", length: 100);
         }
@@ -199,14 +174,17 @@ namespace Balloony
         }
 
         private TouchActionType _touchType;
-        private bool isMoving;
 
         void Handle_TouchAction(object sender, Balloony.TouchEffect.TouchActionEventArgs args)
         {
             _touchType = args.Type;
+            if(this.AnimationIsRunning("FloatAnimation") || this.AnimationIsRunning("DropAnimation"))
+            {
+                return;
+            }
+
             if (_touchType == TouchActionType.Pressed || _touchType == TouchActionType.Entered)
             {
-                // isMoving = true;
                 var floatAnimation = new Animation();
                 floatAnimation.Add(0, 1, new Animation((s) =>
                 {
@@ -228,10 +206,6 @@ namespace Balloony
             Percent = (float)((args.Location.X / balloon_slider.Width) * 100);
         }
 
-
-
-        // Bindable Property - Selected Color - better naming
-        // Bindable Property - UnSelected Color - better naming
         private void DrawSlider(SKCanvas canvas, SKImageInfo info, float percent)
         {
             var y = info.Height - ThumbSize - SelectedThumbThickness; // minus the thumb radius, minus thumb thickness
